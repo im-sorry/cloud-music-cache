5c8c6a026dce367146b34075191641208eea585c
hanqingxin001 <hanqingxin001@gmail.com>
2021-11-24 23:47:02 +0800

    fk

--- src/pages/donation.tsx
+++ src/pages/donation.tsx
@@ -1,11 +1,19 @@
 // @ts-nocheck
 import React from 'react';
-import logo from '../img/logo.png';
+import logo from '../img/fk.jpg';
 
 function App() {
   return (
-    <div className="donation">
-      <img src={logo} alt="" />
+    <div
+      className="donation"
+      style={{
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <img src={logo} alt="" width="300" height="300" />
     </div>
   );
 }
